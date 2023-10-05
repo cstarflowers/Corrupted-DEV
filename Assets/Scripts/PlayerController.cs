e377fa969e9a294a9527de7d56b2db4a0106abba
Carson S <carson.stary@stu.boone.kyschools.us>
2023-10-04 21:35:42 -0400

    Improve animation accuracy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,43 +14,46 @@ void Start() {
     GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
 
     // ORIGINAL CODE BY CG SMOOTHIE (https://www.youtube.com/watch?v=1YKfBh1FCWY)
-    // DO NOT ADD AS CHILD OF PLAYER
+    // WARNING: DO NOT ADD AS CHILD OF PLAYER
 }
 
 public void Update()
 {
+    float VerticalRaw = Input.GetAxisRaw("Vertical");
+    float HorizontalRaw = Input.GetAxisRaw("Horizontal");
     transform.position = Vector3.MoveTowards(transform.position, moveTo.position, speed * Time.deltaTime);
 
     // Continue only if the player's position has changed by greater than the value on the right (0.1f)
     if(Vector3.Distance(transform.position, moveTo.position) <= 0.1f) {
+        animator.SetBool("isWalking", true);
 
         // Check if the player is moving on the Horizontal (X) axis
-        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1) {
+        if(Mathf.Abs(HorizontalRaw) == 1) {
+            // Update animator to reflect change in X-position
+            animator.SetFloat("MoveX", HorizontalRaw);
+            animator.SetFloat("MoveY", 0);
             // Ensure player's desired location is not colliding with an object before continuing
-            if(!Physics2D.OverlapCircle(moveTo.position + new Vector3(Input.GetAxisRaw("Horizontal"),0f), 0.2f,collisionLayer)) {
+            if(!Physics2D.OverlapCircle(moveTo.position + new Vector3(HorizontalRaw, 0f), 0.2f, collisionLayer)) {
                 // Add the player's desired movement to their coordinates, changing position
-                moveTo.position += new Vector3(Input.GetAxisRaw("Horizontal"),0f);
+                moveTo.position += new Vector3(HorizontalRaw, 0f);
             }
         }
 
         // Check if player is instead moving on the Vertical (Y) axis
-        else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1) {
+        else if(Mathf.Abs(VerticalRaw) == 1) {
+            // Update animator to reflect change in Y-position
+            animator.SetFloat("MoveY", VerticalRaw);
+            animator.SetFloat("MoveX", 0);
             // Ensure player's desired location is not colliding with an object before continuing
-            if(!Physics2D.OverlapCircle(moveTo.position + new Vector3(0f, Input.GetAxisRaw("Vertical")), 0.2f,collisionLayer)) {
+            if(!Physics2D.OverlapCircle(moveTo.position + new Vector3(0f, VerticalRaw), 0.2f, collisionLayer)) {
                 // Add the player's desired movement to their coordinates, changing position
-                moveTo.position += new Vector3(0f, Input.GetAxisRaw("Vertical"));
+                moveTo.position += new Vector3(0f, VerticalRaw);
             }
         }
     }
-    if(Mathf.Abs(Input.GetAxisRaw("Vertical")) != 0 || Mathf.Abs(Input.GetAxisRaw("Horizontal")) != 0) {
-        animator.SetBool("walking", true);
-        animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-        animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-    }
-    else {
-        animator.SetBool("walking", false);
-        //animator.SetFloat("MoveY", 999);
-        //animator.SetFloat("MoveX", 999);
+    // If player has stopped moving (no new input), update animator to stop
+    if(Mathf.Abs(VerticalRaw) == 0 && Mathf.Abs(HorizontalRaw) == 0) {
+        animator.SetBool("isWalking", false);
     }
 }
 }
