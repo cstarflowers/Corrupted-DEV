217a9569482ea60e5d5ee007f53e873d7c7f3ab4
Carson S <carson.stary@stu.boone.kyschools.us>
2024-02-19 14:11:43 -0500

    Randomize enemy spawning

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public Rigidbody2D player;
+    public GameObject holdingObj;
     public GameObject projectile;
+    public GameObject healthProj;
     private int projectileCount = 0;
     // Update is called once per frame
     void Update()
     {
-        if(PlayerController.enemyHealth > 0 && projectileCount <= 10) {
-            int randX = Random.Range(-47,10);
-            int randY = Random.Range(40,275);
+        if(player.GetComponent<PlayerController>().enemyHealth > 0 && projectileCount < 10) {
+            int healChance = Random.Range(0,5);
 
-            Vector2 projPos = new Vector2(randX, randY);
-            Instantiate(projectile, projPos, Quaternion.identity);
+            Vector2 projPos = new Vector2(Random.Range(-6,0), Random.Range(4,12));
+            // Debug.Log("spawning at " + projPos.ToString());
+            if(healChance < 4) {
+                StartCoroutine(DelayDestruction(Instantiate(projectile, projPos, Quaternion.identity, holdingObj.transform)));
+            }
+            else {
+                StartCoroutine(DelayDestruction(Instantiate(healthProj, projPos, Quaternion.identity, holdingObj.transform)));
+            }
             projectileCount += 1;
         } 
+    }
+    IEnumerator DelayDestruction(GameObject proj) {
+        yield return new WaitForSeconds(3);
+        Destroy(proj);
+        projectileCount -= 1;
     }
 }
